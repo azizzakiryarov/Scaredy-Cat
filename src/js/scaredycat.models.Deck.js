6bb62657f145add05bd0875308cac40c5ea621af
azizzakiryarov <azizzakiryarov@Azizs-MacBook-Pro.local>
2017-09-06 11:57:27 +0200

    Created arrays

--- src/js/scaredycat.models.Deck.js
+++ src/js/scaredycat.models.Deck.js
@@ -1,9 +1,12 @@
-// eslint class-methods-use-this: "error"
 // eslint no-undef: "error"
+// eslint no-plusplus: "error"
+// eslint no-param-reassign: "error"
+// eslint class-methods-use-this: "error"
+// eslint-env es6
 
-const deck = [];
 scaredycat.models.Deck = class {
   static getWholeDeck() {
+    const deck = [];
     for (let cardBird = 0; cardBird < 41; cardBird = +1) {
       deck.push(new scaredycat.model.Bird());
     }
@@ -13,5 +16,16 @@ scaredycat.models.Deck = class {
     for (let card = 41; card < 47; card = +1) {
       deck.push(new scaredycat.model.ScaredyCat());
     }
+    return deck;
+  }
+
+  static shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
   }
 };
