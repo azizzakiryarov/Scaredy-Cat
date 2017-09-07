b34554ba4d9a2bcef98481cbab74ab2856338c44
azizzakiryarov <azizzakiryarov@Azizs-MacBook-Pro.local>
2017-09-07 11:53:07 +0200

    changed function

--- src/js/scaredycat.game.js
+++ src/js/scaredycat.game.js
@@ -1,10 +1,13 @@
 // eslint no-unused-vars: "error"
+
 scaredycat.game = () => {
-  const hand = [];
-  const table = [];
-  const getDeck = scaredycat.models.Deck.getWholeDeck();
-  const deckTotal = document.querySelector('.deckTotal');
-  const takeACard = hand.push(getDeck.pop());
-  const putACardOnTheTable = table.push(takeACard);
-  deckTotal.innerText(getDeck.length);
+  window.addEventListener('DOMContentLoaded', () => {
+    const hand = [];
+    const table = [];
+    const getDeck = scaredycat.models.Deck.getWholeDeck();
+    const deckTotal = document.querySelector('.deckTotal');
+    const takeACard = hand.push(getDeck.pop());
+    const putACardOnTheTable = table.push(takeACard);
+    deckTotal.innerText(getDeck.length);
+  });
 };
