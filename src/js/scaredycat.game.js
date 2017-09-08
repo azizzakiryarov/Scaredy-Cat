0256122de6f3d5ea2c08fad4cd067e74f4d635ad
azizzakiryarov <azizzakiryarov@Azizs-MacBook-Pro.local>
2017-09-08 12:00:54 +0200

    ändrat allt

--- src/js/scaredycat.game.js
+++ src/js/scaredycat.game.js
@@ -1,13 +1,50 @@
-// eslint no-unused-vars: "error"
+window.addEventListener('DOMContentLoaded', () => {
+  const cards = [];
+  const cardsImg = document.getElementsByClassName('submiddle-element')[0];
+  const totalDeck = document.getElementsByClassName('deckTotal')[0];
 
-scaredycat.game = () => {
-  window.addEventListener('DOMContentLoaded', () => {
+  console.log('Game Started');
+
+  function card(typeOfCard) {
+    this.typeOfCard = typeOfCard;
+  }
+
+  function getAllDeck() {
+    const bird = { name: 'bird' };
+    const cat = { name: 'cat' };
+    const scarecrow = { name: 'scarecrow' };
+
+    // for (let b = 0; b < 41; b += 1) {
+    // for (let c = 41; c < 47; c += 1) {
+    // for (let sc = 47; sc < 50; sc += 1) {
+    // cards.push(card(bird.name), card(cat.name), card(scarecrow.name));
+    cards.push(10);
+    if (cards.length === 0) {
+      console.log('Empty');
+    }
+    // }
+    // }
+    // }
+    return cards;
+  }
+
+  function getACard() {
     const hand = [];
+    hand.push(getAllDeck().pop);
+    return hand;
+  }
+
+  function putACardOnTheTable() {
     const table = [];
-    const getDeck = scaredycat.models.Deck.getWholeDeck();
-    const deckTotal = document.querySelector('.deckTotal');
-    const takeACard = hand.push(getDeck.pop());
-    const putACardOnTheTable = table.push(takeACard);
-    deckTotal.innerText(getDeck.length);
-  });
-};
+    table.push(getACard().pop);
+    return table;
+  }
+
+  function getAOneCard() {
+    getACard();
+    console.log(`I take a card${getACard}`);
+    console.log(cards.length);
+  }
+  cardsImg.addEventListener('click', getAOneCard);
+  totalDeck.write('cards.length');
+});
