7bcfeedc744c6685261d23fb6145c7005aa6ea77
azizzakiryarov <azizzakiryarov@Azizs-MBP.lan>
2017-09-08 21:43:06 +0200

    +

--- src/js/scaredycat.game.js
+++ src/js/scaredycat.game.js
@@ -15,27 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const player1Score = document.querySelector('.score1Summa');
   console.log('Game Started');
 
-
-  /*  
-    function shuffle(array) {
-    const currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-    // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
- */
-
   function getACard() {
     hand.push(cards.pop());
     return hand;
